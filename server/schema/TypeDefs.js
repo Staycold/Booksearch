f046ab9a379822d58398b435f71ce75e10d04595
patrick brodie <patrickxbrodie@hotmail.com>
2021-07-24 22:05:32 -0400

    added the starter code adn filled out typedefs and mutations

---
+++ server/schema/TypeDefs.js
@@ -0,0 +1,51 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!) : Auth
+    saveBook(bookData: BookSearch!) : User
+    removeBook(bookId: ID!) : User
+}
+
+type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Book {
+    bookId: String!
+    title: String!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+}
+ 
+input BookSearch {
+    bookId: String
+    title: String
+    authors: [String]
+    description: String
+    image: String
+    bookLink: String
+}
+`
+
+module.exports = typeDefs;
